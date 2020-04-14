2a992d23717b028026ba0ec2fc94b8f5af2d2981
PierrickMkDev <pierrick.maczkowiak@edu.devinci.fr>
2020-04-14 09:02:11 +0200

    init !

---
+++ Assets/Scripts/RocketBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBehaviour : MonoBehaviour
+{
+	[SerializeField] private Rigidbody rb;
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			
+		}
+	}
+}
